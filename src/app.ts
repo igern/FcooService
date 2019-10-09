bdf6f850f8d05839128ace1ca7ae7965b329f8b5
jonas <jones16511@hotmail.com>
2019-10-09 14:00:51 +0200

    minor changes related to linting

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,63 @@
-import express from 'express';
-import bodyparser from 'body-parser'
-import { PolygonalGeofence } from './geofence'
-import { MongoClient } from 'mongodb'
-import assert = require('assert');
-import {LatLon} from './latlon';
+import assert = require("assert");
+import bodyparser from "body-parser";
+import express from "express";
+import { MongoClient } from "mongodb";
+import { PolygonalGeofence } from "./geofence";
+import { LatLon } from "./latlon";
 
-const dbUrl = process.env.databasePath || "mongodb://localhost:27017"
-const dbName = process.env.dbName || "fcoodb"
+const dbUrl = process.env.databasePath || "mongodb://localhost:27017";
+const dbName = process.env.dbName || "fcoodb";
 const client = new MongoClient(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 const port = process.env.port || "3001";
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     return console.error(err);
   }
   return console.log(`server is listening on ${port}`);
 });
 
-app.post('/api', function(req, res) {
-    let input = req.body;
+app.post("/api", (req, res) => {
+  const input = req.body;
 
-    let coordinateList = [];
-    for(let i = 0; i < input.geofence.latlonList.length; i++) {
-        let point = input.geofence.latlonList[i];
-        coordinateList.push(new LatLon(point.lat, point.lon))
-    }
+  const coordinateList = [];
+  for (const point of input.geofence.latlonList) {
+    coordinateList.push(new LatLon(point.lat, point.lon));
+  }
 
-    let geofence;
-    switch(input.geofence.type) {
-        case "polygon":
-            geofence = new PolygonalGeofence(input.beginTime, input.endTime, coordinateList)
-            break;
-        default:
-            res.send("bad geofence type")
-            break;
-    }
+  let geofence;
+  switch (input.geofence.type) {
+    case "polygon":
+      geofence = new PolygonalGeofence(
+        input.beginTime,
+        input.endTime,
+        coordinateList,
+      );
+      break;
+    default:
+      res.send("bad geofence type");
+      break;
+  }
 
+  client.connect(async (err) => {
+    assert.equal(null, err);
 
-    client.connect(async function(err) {
-        assert.equal(null, err);
-
-        const db = client.db(dbName)
-        geofence.handle(db).then((result) => {
-            client.close();
-            if(result.length < 1) {
-                res.status(404).send("no data")
-            } else {
-                res.status(200).send(result)
-            }
-        })
-    })
-})
+    const db = client.db(dbName);
+    geofence.handle(db).then((result) => {
+      client.close();
+      if (result.length < 1) {
+        res.status(404).send("no data");
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  });
+});
